5ff6ae515d10688b4ebafbcf06472955b10171ee
Connor McCormack <mccormack.connor.m@gmail.com>
2020-09-25 00:31:15 -0500

    first commit 26 mins

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import useFetchJobs from './useFetchJobs'
+import { Container } from 'react-bootstrap'
+import Job from './Job'
 
 function App() {
+  const [params, setParams] = useState({})
+  const [page, setPage] = useState(1)
+  const { jobs, loading, error } = useFetchJobs(params, page)
+
   return (
-    <main className='App'>
-      {/* content goes here */}
-    </main>
+    <Container>
+      {loading && <h1>Loading...</h1>}
+      {error && <h1>Error. Try refreshing!</h1>}
+      {<h1>{jobs.map(job => {
+        return <Job key={job.id} job={job} />
+      })}</h1>}
+    </Container>
   );
 }
 
